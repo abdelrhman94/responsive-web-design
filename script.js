228afbb24b7bbd0588a3246ef1d4733336ea8c86
Abdelrhman Saied <abdelrhman94@github.com>
2020-05-06 03:10:18 +0200

    design2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const selectElement = (s) => document.querySelector(s);
 
-selectElement(".open").addEventListener("click", () => {
-  selectElement(".nav-list").classList.add("active");
+selectElement('.open').addEventListener('click', () => {
+  selectElement('.nav-list').classList.add('active');
 });
 
-selectElement(".close").addEventListener("click", () => {
-  selectElement(".nav-list").classList.remove("active");
+selectElement('.close').addEventListener('click', () => {
+  selectElement('.nav-list').classList.remove('active');
 });
